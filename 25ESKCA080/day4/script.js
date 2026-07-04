fd5f7a3ab33aaf04169d022a766364a9961b40bf
Garvit joshi <joshigarvit808@gmail.com>
2026-07-04 16:02:07 +0530

    first commit

---
+++ 25ESKCA080/day4/script.js
@@ -0,0 +1,7 @@
+    const toggleButton=document.getElementById("toggle");
+    toggleButton.addEventListener("click",function(){
+     document.body.classList.toggle("bg-dark")
+     document.body.classList.toggle("text-white")
+    });
+    
+    console.log("JavaScript Connected!");
